40da27820e5cc149cfd48f160a48831fdb9c05c8
root <adnamantino@outlook.com>
2015-03-20 00:02:52 +0100

    Cambios Interface

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <aside>
 	<!-- Todo el menu de navegacion -->
 	<ul>
-		<li><a href="./index.php"><i class="fa fa-home"> </i> Portada</a></li>
+		<li><a href="./index.php" class="active"><i class="fa fa-home"> </i> Portada</a></li>
 		<li><a href="#"><i class="fa fa-globe"> </i> Proximas Carreras</a></li>
 		<li><a href="./nuevousuario.php"><i class="fa fa-user-plus"> </i> Nuevos Usuarios</a></li>
 		<li><a href="./organizadores.php"><i class="fa fa-cube"> </i> Organizadores</a></li>
