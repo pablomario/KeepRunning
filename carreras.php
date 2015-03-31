bdb7d85826e1f88272e3a88f7962d443a5bbd197
adnamantino@outlook.com <adnamantino@outlook.com>
2015-03-31 11:59:10 +0200

    angular ajax empezando single

--- carreras.php
+++ carreras.php
@@ -2,7 +2,8 @@
 	<!-- en formato cartel poner todas las carreras disponibles -->
 	<?php 
 		for($i = 0; $i < 14; $i++){
-			echo '<article class="cartel"> <img src="./imgs/carreras/cartel.png"> <h4> I Carrera del Torrezno </h4> <p>Inscripcion Cerrada</p></article>';
+			echo '<article class="cartel"> <img src="./imgs/carreras/cartel.png"> <a href="http://127.0.0.1/pmKeep/#/single/'.$i.'">
+			<h4> I Carrera del Torrezno </h4></a> <p>Inscripcion Cerrada</p></article>';
 		}
 	?>
 
--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ var app = angular.module("app", ["ngRoute"]);
 
 app.config(['$routeProvider', function($routeProvider)
 {
-    $routeProvider.when("/carreras", 
+    $routeProvider.when("/carreras",
     {
         templateUrl : "carreras.php",
         controller : "carreras"
     })
-    .when("/nuevousuario", 
+    .when("/nuevousuario",
     {
         templateUrl : "nuevousuario.php",
         controller : "nuevousuario"
@@ -28,44 +28,50 @@ app.config(['$routeProvider', function($routeProvider)
         templateUrl : "contacto.php",
         controller : "contacto"
     })
+     .when("/single/:carreraId",{
+        templateUrl : "single.php",
+        controller : "single"
+     })
     .otherwise({ redirectTo : "/" });
 }]);
 
+app.controller('single', ['$scope', '$routeParams' , function($scope,$routeParams){
+    $scope.variable = "single";
+    $scope.carreraId = $routeParams.carreraId;
+}])
 
 app.controller('carreras', ['$scope', function($scope)
 {
-	$scope.variable = "careras";
-	
+	$scope.variable = "carreras";
+
 }]);
 
 app.controller('nuevousuario', ['$scope', function($scope)
 {
 	$scope.variable = "nuevousuario";
-	
+
 }]);
 
 app.controller('organziadores', ['$scope', function($scope)
 {
 	$scope.variable = "organizadores";
-	
+
 }])
 
 app.controller('ayuda', ['$scope', function($scope)
 {
     $scope.variable = "ayuda";
-    
+
 }]);
 
 app.controller('contacto', ['$scope', function($scope)
 {
     $scope.variable = "contacto";
-    
+
 }]);
 
 
-
-
-// SECCIONES DEL MENU 
+// SECCIONES DEL MENU
 app.factory('enlacesMenu', function(){
     return{
         menu : [
@@ -76,10 +82,23 @@ app.factory('enlacesMenu', function(){
             {nombre:"Ayuda",             url: "ayuda",          icon:"fa fa-life-ring"},
             {nombre:"Contacto",          url: "contacto",       icon:"fa fa-comments"}
         ]
-    }; 
+    };
 });
 
 app.controller('enlacesmenu', ['$scope', 'enlacesMenu', function($scope,enlacesMenu){
-    $scope.menu = enlacesMenu.menu;  
+    $scope.menu = enlacesMenu.menu;
     $scope.caca = "AAAAAAAAAAAAAAAAAAAAAA";
 }])
+
+
+// AJAX
+
+
+
+app.controller('dcAjax', ['$scope','$http', function($scope,$http){
+
+    $http.get("lib/ajaxphp.php").success(function(textoP){
+        $scope.resultado = textoP;
+    });
+
+}]);
