091e69b210510a0aa03e1207c9063e099e504056
adnamantino@outlook.com <adnamantino@outlook.com>
2015-04-26 20:17:44 +0200

    Mejoras en diseño

---
+++ privacidad.php
@@ -0,0 +1,83 @@
+
+
+
+<section id="section_single">
+
+<style type="text/css">
+	#cabecera_single{
+		background: url(./imgs/portada/otr.png) center center;
+		background-size: cover;
+	}
+
+</style>
+
+	<!-- en formato cartel poner todas las carreras disponibles -->
+	<div id="cabecera_single"></div>
+	<header id="header_single">
+		<div class="contenido_single">
+			<h1>Política de privacidad y aviso legal</h1> 
+		</div>
+		
+		<aside class="lateral_single">
+			
+		</aside>
+	</header>
+	<article id="article_single"> 
+		<div class="contenido_single">
+			<h2>Política de privacidad:</h2>
+			<p>
+
+A través de este sitio web no se recaban datos de carácter personal de los usuarios sin su conocimiento, ni se ceden a terceros.
+
+Con la finalidad de ofrecerle el mejor servicio y con el objeto de facilitar el uso, se analizan el número de páginas visitadas, el número de visitas, así como la actividad de los visitantes y su frecuencia de utilización. A estos efectos, la AEPD utiliza la información estadística elaborada por el Proveedor de Servicios de Internet.
+
+La AEPD no utiliza cookies para recoger información de los usuarios, ni registra las direcciones IP de acceso. Únicamente se utilizan cookies propias,  de sesión, con finalidad técnica (aquellas que permiten al usuario la navegación a través del sitio web y la utilización de las diferentes opciones y servicios que en ella existen).
+
+En cumplimiento de lo previsto por el artículo 5 de la LOPD, se informa que los datos de carácter personal que se recaban directamente del usuario a través de los distintos recursos disponibles en el portal, serán tratados de forma confidencial y quedarán incorporados a los respectivos ficheros de los que es responsable la AEPD, con las finalidades que se detallan en el Registro General de Protección de Datos: Ficheros, no siendo utilizados para finalidades incompatibles con estas. Los datos recabados podrán ser cedidos en los casos previstos, para cada fichero, en dicho Registro.
+
+Los afectados pueden ejercitar los derechos de acceso, rectificación, cancelación y oposición ante la AEPD, mediante escrito dirigido a su Secretaría General, en la calle Jorge Juan, nº 6, 28001 Madrid.
+</p><h2>Aviso legal:</h2><p>
+
+Este portal, cuyo titular es la Agencia Española de Protección de Datos (AEPD), con NIF Q2813014D, domicilio en la calle Jorge Juan, nº6, 28001 Madrid, y teléfono 901 100 099, está constituido por los sitios web asociados a los dominios agpd.es, sedeagpd.gob.es, tudecideseninternet.es y redipd.org.
+
+Propiedad intelectual e industrial
+
+El diseño del portal y sus códigos fuente, así como los logos, marcas y demás signos distintivos que aparecen en el mismo pertenecen a la AEPD y están protegidos por los correspondientes derechos de propiedad intelectual e industrial.
+
+Responsabilidad de los contenidos
+
+La AEPD no se hace responsable de la legalidad de otros sitios web de terceros desde los que pueda accederse al portal. La AEPD tampoco responde por la legalidad de otros sitios web de terceros, que pudieran estar vinculados o enlazados desde este portal.
+
+La AEPD se reserva el derecho a realizar cambios en el sitio web sin previo aviso, al objeto de mantener actualizada su información, añadiendo, modificando, corrigiendo o eliminando  los contenidos publicados o el diseño del portal.
+
+La AEPD no será responsable del uso que terceros hagan de la información publicada en el portal, ni tampoco de los daños sufridos o pérdidas económicas que, de forma directa o indirecta, produzcan o puedan producir perjuicios económicos, materiales o sobre datos, provocados por el uso de dicha información.
+
+Reproducción de contenidos
+
+Se prohíbe la reproducción total o parcial de los contenidos publicados en el portal. No obstante, los contenidos que sean considerados como datos abiertos en la Sede Electrónica, publicados según lo previsto en el Real Decreto 1495/2011, de 24 de octubre, de desarrollo de la Ley 37/2007, sobre reutilización de la información del sector público, para el ámbito del sector público estatal, podrán ser objeto de reproducción en los términos contenidos en el siguiente Aviso.
+
+Registro General de Protección de Datos
+
+La información sobre ficheros inscritos en el Registro General de Protección de Datos se publica de acuerdo con lo previsto en el artículo 14 de la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal (LOPD).
+
+La reproducción total o parcial, incluso el volcado del contenido del Registro a cualquier soporte, exigirá la expresa autorización de la AEPD.
+
+Los datos de carácter personal incluidos en el Registro no podrán ser objeto de tratamiento, ni usarse para finalidades distintas a la de esta publicación.
+
+Sede Electrónica
+
+De acuerdo con lo establecido por el artículo 7 del Real Decreto 1671/2009, de 6 de noviembre, por el que se desarrolla parcialmente la Ley 11/2007, de 22 de junio, de acceso electrónico de los ciudadanos a los servicios públicos, la AEPD se responsabiliza de la integridad, veracidad y actualización de la información y los servicios a los que pueda accederse a través de su Sede Electrónica (https://sedeagpd.gob.es/).
+
+Portal de transparencia
+
+A través de la información publicada en el portal de transparencia, la AEPD atiende de forma periódica y actualizada el principio de publicidad activa establecido por la Ley 19/2013, de 9 de diciembre, de transparencia, acceso a la información pública y buen gobierno, con los mecanismos adecuados para facilitar la accesibilidad, la interoperabilidad, la calidad y la reutilización de la información, así como su identificación y localización.
+
+Ley aplicable
+
+La ley aplicable en caso de disputa o conflicto de interpretación de los términos que conforman este aviso legal, así como cualquier cuestión relacionada con los servicios del presente portal, será la ley española.
+	</p>
+			
+		</div>
+	</article>
+
+</section>
